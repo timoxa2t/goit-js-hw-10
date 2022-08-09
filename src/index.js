0fed13c727361900310f5532421949595c48dfcc
timoxa2t <alex2t@ukr.net>
2022-08-09 22:45:32 +0300

    t

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import fetchCountries from "./js/fetchCountries.js"
 
 const DEBOUNCE_DELAY = 300;
 
+
 const searchBoxEl = document.querySelector("#search-box")
 const countryListEl = document.querySelector(".country-list")
 const countryInfoEl = document.querySelector(".country-info")
